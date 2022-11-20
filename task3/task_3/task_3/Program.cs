6fe32ea9a97ae0b51756e06aa43cbb95789ed294
Sofia Soyka <sophia.soyka@gmail.com>
2022-11-20 22:13:32 +0300

    требует проверки

---
+++ task3/task_3/task_3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace task_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, l, n, u, m;
+            double eps = 0.001;
+
+            Console.WriteLine("Введите значение переменной a");
+            double.TryParse(Console.ReadLine(), out a);
+            Console.WriteLine("Введите значение переменной l");
+            double.TryParse(Console.ReadLine(), out l);
+
+            if (a == l)
+                n = a;
+            else
+            {
+                if (a < l)
+                    n = Math.Cos(a);
+                else
+                    n = a * Math.Sin(a);
+            }
+
+            if (n > 5)
+                u = 5;
+            else
+            {
+                if (n == 5)
+                    u = 5 + n * 6;
+                else
+                    u = Math.Cos(n);
+            }
+
+            if (Math.Abs(u * u + n) > eps)
+                Console.WriteLine("Значение выражения = " + (Math.Cos(u) - u) / (u * u + n));
+            else
+                Console.WriteLine("Делить на ноль нельзя");
+            Console.ReadKey();
+        }
+    }
+}
