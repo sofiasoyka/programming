0cea2d1df9ea851dbd498a4b91d4fe1cc478882f
Sofia Soyka <sophia.soyka@gmail.com>
2022-11-24 21:09:03 +0300

    в процессе исправления маленьких нюансов оформления

---
+++ task6/task_6/task_6/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace task_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, x, b_0, b_k, delta_b, G;
+            double eps = 0.001;
+
+            Console.WriteLine("Введите значение переменной а");
+            double.TryParse(Console.ReadLine(), out a);
+            Console.WriteLine("Введите значение переменной x");
+            double.TryParse(Console.ReadLine(), out x);
+            Console.WriteLine("Введите начальное значение b");
+            double.TryParse(Console.ReadLine(), out b_0);
+            Console.WriteLine("Введите конечное значение b");
+            double.TryParse(Console.ReadLine(), out b_k);
+            Console.WriteLine("Введите значение шага");
+            double.TryParse(Console.ReadLine(), out delta_b);
+
+            for (double i = b_0; b_0 <= b_k;)
+            {
+                if ((Math.Abs(b_0 * b_0 - x) <= eps) ^ (Math.Abs(Math.Exp(a) + b_0) <= eps))
+                    Console.WriteLine("При b = {0}, значение G невозможно вычислить", b_0);
+                else
+                {
+                    G = (2.4 * Math.Cos(Math.Pow(a, 3) + Math.Pow(b_0, a)) + Math.Pow(b_0 * b_0 - x, 1 / 3)) / (Math.Exp(a) + b_0);
+                    if (Math.Abs(G) <= eps)
+                        Console.WriteLine("При b = {0}, значение G = 0", b_0);
+                    else
+                        Console.WriteLine("При b = {0}, значение G = {1}", b_0, G);
+                }
+                b_0 += delta_b;
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ task7/task_7/task_7/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace task_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            double w, x, F, F1; // F1 - вспомогательная переменная для счета F
+
+            Console.WriteLine("Введите количество эелементов ряда");
+            int.TryParse(Console.ReadLine(), out n);
+            Console.WriteLine("Введите значение переменной w");
+            double.TryParse(Console.ReadLine(), out w);
+            Console.WriteLine("Введите значение переменной x");
+            double.TryParse(Console.ReadLine(), out x);
+
+            if (n == 1)
+            {
+                F = (0.5 * w + Math.Sqrt(x * x + 5)) / (Math.Sin(1 * 2 * 3 * 4) + 3);
+                Console.WriteLine("Значение F = " + F);
+            }
+
+            else
+            {
+                // переменные в выражении
+                double v1 = 0.5;
+                double v2 = x * x;
+                int v3 = 5;
+                int v4 = 1 * 2 * 3 * 4;
+                int v5 = 3;
+
+                F = 0;
+
+                // вспомогательные переменные для v2
+                int k2 = 3;
+                int k2_1 = 2;
+                int k2_2 = 3;
+
+                int k4 = 5; // вспомогательная переменная для v4
+
+                for (int i = 1; i <= n; i++)
+                {
+                    F1 = (v1 * w + Math.Sqrt(v2 + v3)) / (Math.Sin(v4) + v5);
+                    F += F1;
+                    v1 += 0.2;
+                    v2 = Math.Pow(x, k2);
+                    k2 = k2_1 * k2_2;
+                    k2_1 = k2 / k2_1;
+                    k2_2 = k2;
+                    v3 += 5;
+                    v4 *= k4;
+                    k4 += 1;
+                    v5 += 3;
+                }
+
+                Console.WriteLine("Значение F = " + F);
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
